0470a4f3f1d06baece724ed1db32e27065c16044
Caglar <caglarsulun@hotmail.com>
2021-11-22 23:22:24 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+const num3 = 9;
+const num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(5, 7, 9, 12, 16, 18, 20);
+
+//extra practice
+
+const random1 = Math.floor(Math.random() * 100);
+const random2 = Math.floor(Math.random() * 100);
+const random3 = Math.floor(Math.random() * 100);
+
+console.log(`1st random number is ${random1}`);
+console.log(`2nd random number is ${random2}`);
+console.log(`3rd random number is ${random3}`);
+
+const maximum = Math.max(random1, random2, random3);
+
+console.log(
+  `Among these (${random1}, ${random2}, ${random3}) numbers; max number is ${maximum}`
+);
+
+console.log(random);
+console.log(mod);
+console.log(max);
