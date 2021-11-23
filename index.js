7d906c8590432bf6caf0e6cfcebc645f03ede164
Caglar <caglarsulun@hotmail.com>
2021-11-22 23:14:21 -0500

    commit tree c14ba1e6e0724c861197454362733dd4e5676364

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2; 
+
+const random = Math.floor(Math.random()*10);
+
+const num3 = 9;
+const num4 = 5;
+
+const mod = num3 % num4;
+
+console.log(random);
+console.log(mod);
+
+const max = Math.max(5, 7, 9, 12, 16, 18, 20);
+console.log(max);
